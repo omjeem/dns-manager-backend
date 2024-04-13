722342bee7c2321a423992605a4ee55735b9d784
Om Jee Mishra <93421142+om-jee-m@users.noreply.github.com>
2024-04-14 00:40:04 +0530

    add health check route

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,10 @@ app.use("/user", userRouter)
 app.use("/domain", dnsDomainRouter)
 app.use("/record", dnsRecordRouter)
 
+app.get("/", (req, res) => {
+    return res.status(200).json("Health Check");
+})
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`)
